0c3931ef286c182e3134907f670d562db99f7780
WhiteMusk <48273698+WhiteMusk@users.noreply.github.com>
2021-03-31 14:53:13 +0800

    Update gui_new.py

--- gui_new.py
+++ gui_new.py
@@ -4,9 +4,9 @@ import pygame_gui
 pygame.init()
 #music setting
 pygame.mixer.init()
-#pygame.time.delay(1000)#等待1秒讓mixer完成初始化
+#pygame.time.delay(1000) #wait for 1 sec. so that the initialization can be done.
 
-pygame.display.set_caption('阿賢人生')
+pygame.display.set_caption('Your Digital Raio') #users may rename the GUI caption by themselves.
 screen_width=1980
 screen_height=1500
 window_surface = pygame.display.set_mode([screen_width, screen_height])
@@ -20,15 +20,17 @@ play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 375),
 slider_A = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(relative_rect=pygame.Rect((900, 425), (800, 50)),manager=manager,value_range=(0.0,1.0),start_value=0.0)
 slider_B = pygame_gui.elements.ui_horizontal_slider.UIHorizontalSlider(relative_rect=pygame.Rect((900, 500), (800, 50)),manager=manager,value_range=(0.0,1.0),start_value=0.0)
 
-#Option List
-button1 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 125), (100, 50)), text='Tape 1', manager=manager) #campus
-button2 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 225), (100, 50)), text='Tape 2', manager=manager) #date
-button3 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 325), (100, 50)), text='Tape 3', manager=manager) #office
-button4 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 425), (100, 50)), text='Tape 4', manager=manager) #elevator
-button5 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 525), (100, 50)), text='Tape 5', manager=manager) #tunnel
+#Option List: used if there are more than one sets of soundtracks
+button1 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 125), (100, 50)), text='Tape 1', manager=manager) #soundtrack set 1
+button2 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 225), (100, 50)), text='Tape 2', manager=manager) #soundtrack set 2
+button3 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 325), (100, 50)), text='Tape 3', manager=manager) #soundtrack set 3
+button4 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 425), (100, 50)), text='Tape 4', manager=manager) #soundtrack set 4
+button5 = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 525), (100, 50)), text='Tape 5', manager=manager) #soundtrack set 5
 
-music1 = pygame.mixer.Sound('A.wav')
-music2 = pygame.mixer.Sound('B.wav')
+#default music: you may fill in your first set of soundtracks
+#the format should be .wav files.
+music1 = pygame.mixer.Sound('soundtrackFileName')
+music2 = pygame.mixer.Sound('soundtrackFileName')
 
 
 clock = pygame.time.Clock()
@@ -41,11 +43,9 @@ while is_running:
         if event.type == pygame.QUIT:
             is_running = False
 
-        
-
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                #muisc opt
+                #muisc opt: file names are just for your reference. You may adapt them into the ones you're using.
                 if event.ui_element == button1:
                     music1 = pygame.mixer.Sound('campusA.wav')
                     music2 = pygame.mixer.Sound('campusB.wav')
@@ -70,7 +70,7 @@ while is_running:
                     music2.set_volume(slider_B.get_current_value())
 
             if event.user_type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
-                #volume
+                #volume: the slide bar for adapting the volumes of the soundtracks separately.
                 if event.ui_element == slider_A or  event.ui_element == slider_B:                  
                     music1.set_volume(slider_A.get_current_value())
                     music2.set_volume(slider_B.get_current_value())
